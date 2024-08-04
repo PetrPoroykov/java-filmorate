b10fdb8a7e4e87e6010abb2a55b18e5259d01a75
Petr Poroykov <pporoykov@gmail.com>
2024-08-04 11:50:50 +0300

    Еще испрвылены стилистические ошибки

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -6,7 +6,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 @Component
-@Data
 public class FilmService {
 
 
--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -1,16 +1,11 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.*;
-import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.controller.UserController;
-import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
